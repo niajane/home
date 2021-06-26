8acd6e210199879064935ccdabc4d6cbdeeefa21
Nia Gardner <5283851+niajane@users.noreply.github.com>
2021-06-26 13:33:31 +0100

    view list names, onclick go to list view

--- client/screens/TabOneScreen.tsx
+++ client/screens/TabOneScreen.tsx
@@ -1,17 +1,54 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState , useEffect } from 'react';
+import * as api from '../api/index';
+import { ActivityIndicator, FlatList, StyleSheet } from 'react-native';
 
 import TodoList from '../components/TodoList';
 import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
+  
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState<any[]>([]);
+  const [currentList, setCurrentList] = useState('');
+
+  useEffect(() => {
+    if (!currentList) {
+      api.getListNames()
+        .then((response) => setData(response.data))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }
+    else {
+      api.getList(currentList)
+        .then((response) => setData(response.data))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }
+     
+  }, []);
+
+  if (currentList) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Grocery</Text>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <TodoList listName="Grocery"/>
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Grocery</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <TodoList listName="Grocery"/>
-    </View>
-  ); 
+      <View style={styles.container}>
+          {isLoading ? <ActivityIndicator/> : (
+              <FlatList
+                  data={data}
+                  keyExtractor={(index) => index}
+                  renderItem={(item) => (
+                      <Text onPress={() => setCurrentList(item.item)}>{item.item}</Text>
+                  )}
+              />
+          )}
+      </View>
+);
 }
 
 const styles = StyleSheet.create({
@@ -30,3 +67,4 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
